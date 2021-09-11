d6343cc9f499d4622067d40ac79a113c1a9bb226
Pmyl <julo134@gmail.com>
2021-09-11 17:46:06 +0100

    not even trying

--- pran-echo/frontend/src/index.ts
+++ pran-echo/frontend/src/index.ts
@@ -3,6 +3,8 @@ import 'pran-animation-editor-frontend/index.css';
 import { Container, createAnimationEditor, Modal } from 'pran-animation-editor-frontend';
 import { createEchoPanel } from './components/echo-panel/echo-panel';
 
+console.log('v1.0.0');
+
 document.addEventListener('DOMContentLoaded', async () => {
   const body: Container = Container.CreateBody();
   Modal.init(body);
